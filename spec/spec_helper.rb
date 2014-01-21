6c09f01817149ee92b8db6a56a04e0d99d4107de
Михаил Кузьмин <m.kuzmin@darkleaf.ru>
2014-01-21 21:00:52 +0400

    fix spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,48 +19,48 @@ Spork.prefork do
   Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
   ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
+end
 
-  RSpec.configure do |config|
-    # ## Mock Framework
-    #
-    # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-    #
-    # config.mock_with :mocha
-    # config.mock_with :flexmock
-    # config.mock_with :rr
+RSpec.configure do |config|
+  # ## Mock Framework
+  #
+  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
+  #
+  # config.mock_with :mocha
+  # config.mock_with :flexmock
+  # config.mock_with :rr
 
-    # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-    config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-    # If you're not using ActiveRecord, or you'd prefer not to run each of your
-    # examples within a transaction, remove the following line or assign false
-    # instead of true.
-    config.use_transactional_fixtures = true
+  # If you're not using ActiveRecord, or you'd prefer not to run each of your
+  # examples within a transaction, remove the following line or assign false
+  # instead of true.
+  config.use_transactional_fixtures = true
 
-    # If true, the base class of anonymous controllers will be inferred
-    # automatically. This will be the default behavior in future versions of
-    # rspec-rails.
-    config.infer_base_class_for_anonymous_controllers = false
+  # If true, the base class of anonymous controllers will be inferred
+  # automatically. This will be the default behavior in future versions of
+  # rspec-rails.
+  config.infer_base_class_for_anonymous_controllers = false
 
-    # Run specs in random order to surface order dependencies. If you find an
-    # order dependency and want to debug it, you can fix the order by providing
-    # the seed, which is printed after each run.
-    #     --seed 1234
-    config.order = "random"
+  # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = "random"
 
-    config.before :all do
-      BaseSearchRepository.remove_all_indicies
-      ElasticsearchSchema.create_indices
-      ElasticsearchSchema.put_mappings
-    end
+  config.before :all do
+    BaseSearchRepository.remove_all_indicies
+    ElasticsearchSchema.create_indices
+    ElasticsearchSchema.put_mappings
+  end
 
-    config.after :each do
-      BaseSearchRepository.remove_all_documents
-    end
+  config.after :each do
+    BaseSearchRepository.remove_all_documents
+  end
 
-    config.after :all do
-      BaseSearchRepository.remove_all_indicies
-    end
+  config.after :all do
+    BaseSearchRepository.remove_all_indicies
   end
 end
 
