45194f22935bf5285557b94bcec9075fdcd8013d
Михаил Кузьмин <m.kuzmin@darkleaf.ru>
2014-02-01 20:56:34 +0400

    update

--- app/controllers/api/v1/stars_controller.rb
+++ app/controllers/api/v1/stars_controller.rb
@@ -3,6 +3,8 @@ class Api::V1::StarsController < ApplicationController
     url = star_params[:url]
     StarCreatorService.create(url)
 
+    #TODO: разобраться
+    response.headers['Access-Control-Allow-Origin'] = '*'
     head :ok
   end
 
