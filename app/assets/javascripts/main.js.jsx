844a50454eea7c547e421b60d25d612ff4b8c941
Михаил Кузьмин <m.kuzmin@darkleaf.ru>
2014-02-22 11:29:55 +0400

    add css

--- app/assets/javascripts/main.js.jsx
+++ app/assets/javascripts/main.js.jsx
@@ -3,12 +3,14 @@
 var SearchForm = React.createClass({
   render: function() {
     return (
-      <form onSubmit={this.props.handleSubmit}>
-        <div className='form-group'>
-          <input onChange={this.props.onChange} className='form-control' value={this.props.query} />
-        </div>
-        <button className='btn btn-default'>Search</button>
-      </form>
+      <div className='search-form'>
+        <form onSubmit={this.props.handleSubmit}>
+          <div className='form-group'>
+            <input onChange={this.props.onChange} className='form-control' value={this.props.query} />
+          </div>
+          <button className='btn btn-default'>Search</button>
+        </form>
+      </div>
     );
   }
 });
