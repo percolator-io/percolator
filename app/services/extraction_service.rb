f127d5ac91906250e378add8a5f6055a703d947a
Михаил Кузьмин <m.kuzmin@darkleaf.ru>
2014-02-27 08:29:17 +0400

    update extraction_service

--- app/services/extraction_service.rb
+++ app/services/extraction_service.rb
@@ -1,22 +1,35 @@
 module ExtractionService
   class << self
     def extract(page)
-      attrs = {}
-      attrs[:title] = page.title
-      attrs[:description] = page.search('/html/head/meta[@name="description"]/@content').first.try(:value)
+      {
+          title:  page.title,
+          description: description(page),
+          keywords: keywords(page),
+          content: content(page),
+      }
+    end
 
-      keywords = page.search('/html/head/meta[@name="keywords"]/@content').first.try(:value) || ''
-      attrs[:keywords] = keywords.split(',').map(&:strip)
+  private
+    def description(page)
+      max_length = 160
+      descr = page.search('/html/head/meta[@name="description"]/@content').first.try(:value) || ''
 
+      return descr if descr.length > 0 && descr.length < max_length
+      content(page).truncate(max_length, separator: /\s/)
+    end
+
+    def content(page)
       document = Readability::Document.new(page.content)
       sanitizer = HTML::FullSanitizer.new
       sanitized_content = sanitizer.sanitize(document.content)
       sanitized_content.gsub!(/\s+/, ' ' )
       sanitized_content.strip!
+      sanitized_content
+    end
 
-      attrs[:content] = sanitized_content
-
-      attrs
+    def keywords(page)
+      keywords = page.search('/html/head/meta[@name="keywords"]/@content').first.try(:value) || ''
+      keywords.split(',').map(&:strip)
     end
   end
 end
