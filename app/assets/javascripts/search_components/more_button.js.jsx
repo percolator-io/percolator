3beaa485a65e56ba946cd799a373ff04de8f4f0d
Михаил Кузьмин <m.kuzmin@darkleaf.ru>
2014-03-02 23:37:00 +0400

    link instead button

--- app/assets/javascripts/search_components/more_button.js.jsx
+++ app/assets/javascripts/search_components/more_button.js.jsx
@@ -2,7 +2,7 @@
 
 var MoreButton = React.createClass({
   button: function(){
-    return <button type="button" className="btn btn-default btn-block" onClick={this.props.handler}>Get more</button>;
+    return <a className="btn btn-default btn-block" onClick={this.props.handler}>Get more</a>;
   },
 
   fallback: function(){
