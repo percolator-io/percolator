198e60a6a5146b9d2e55e1e6690576e63be05413
Михаил Кузьмин <m.kuzmin@darkleaf.ru>
2014-03-09 12:07:48 +0400

    add runit

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,7 +15,7 @@ set :deploy_to, '/var/www/percolator'
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml config/application.yml}
+set :linked_files, %w{config/database.yml config/application.yml config/sidekiq.yml config/unicorn.rb}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
@@ -31,16 +31,15 @@ set :ssh_options, forward_agent: true
 
 namespace :deploy do
 
-  #desc 'Restart application'
-  #task :restart do
-  #  on roles(:app), in: :sequence, wait: 5 do
-  #    # Your restart mechanism here, for example:
-  #    # execute :touch, release_path.join('tmp/restart.txt')
-  #  end
-  #end
-  #
-  #after :publishing, :restart
-  #
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :sv, "restart", "unicorn"
+      execute :sv, "restart", "sidekiq"
+    end
+  end
+  after :publishing, :restart
+
   #after :restart, :clear_cache do
   #  on roles(:web), in: :groups, limit: 3, wait: 10 do
   #    # Here we can do anything such as:
