e6f871e13a2a547fb79291e4f19adb20cf10409a
Михаил Кузьмин <m.kuzmin@darkleaf.ru>
2014-03-11 21:33:13 +0400

    hide secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Asearcher::Application.config.secret_key_base = '6b9b4d6df81860a8e35db6f9c1dc14fb51cc12f7076e31c3a37dbcb3df851dc8251fc43567b2567411816b02154de63e62312802ea38bdfca51c14dac88f9c2e'
+Asearcher::Application.config.secret_key_base = Figaro.env.secret_token
