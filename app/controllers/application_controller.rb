21231fc2ffd873bf358510c8bf2b9bcad5c99c42
Михаил Кузьмин <m.kuzmin@darkleaf.ru>
2014-03-16 09:35:50 +0400

    disable paper_trail

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,3 @@
 class ApplicationController < ActionController::Base
   include UrlHelpers
-
-  def user_for_paper_trail
-    current_user.id
-  end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,4 @@
 class Category < ActiveRecord::Base
   validates :name, presence: true
   acts_as_tree order: 'name'
-  has_paper_trail
 end
