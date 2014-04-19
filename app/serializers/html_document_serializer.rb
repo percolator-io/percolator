b1c6d0454be8fc4109a36e5f0a94f96d8ffd625c
Михаил Кузьмин <m.kuzmin@darkleaf.ru>
2014-04-19 22:19:17 +0400

    fix

--- app/serializers/html_document_serializer.rb
+++ app/serializers/html_document_serializer.rb
@@ -12,8 +12,23 @@ class HtmlDocumentSerializer < ActiveModel::Serializer
     attributes :id, :name
   end
 
+  extend MethodDecorators
+
   attributes :id, :url, :host, :title, :description, :keywords
 
   has_many :categories, serializer: CategorySerializer
   has_many :stars, serializer: StarSerializer
+
+  def title
+    sanitizer.sanitize object.title, tags: %w[em]
+  end
+
+  def description
+    sanitizer.sanitize object.description, tags: %w[em]
+  end
+
+  +MethodDecorators::Memoize
+  def sanitizer
+    HTML::WhiteListSanitizer.new
+  end
 end
